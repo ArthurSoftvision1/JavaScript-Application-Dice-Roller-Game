db5879ade0f6f72183fb50b54eeb26ebb9669a7a
arthur_molnar <amolnar@celerity.com>
2019-04-23 18:55:23 +0300

    Create the numbers from the dices

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ const output = document.querySelector("output");
 
 button.addEventListener("click", function() {
     console.log("works");
+    let rolls = [roll(6),roll(6)];
+    console.log(rolls);
 });
+
+function roll(num) {
+    let rNumber = Math.floor(Math.random()*num) + 1;
+    return rNumber;
+}
